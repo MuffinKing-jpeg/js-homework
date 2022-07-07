c475d83431e1a3489437b9147adb479e1b01f15e
MuffinKing-jpeg <andriydobrus@gmail.com>
2022-07-07 15:27:25 +0300

    Init commit

---
+++ log.override.js
@@ -0,0 +1,15 @@
+(function () {
+    let old = console.log;
+    let logger = document.getElementById('log');
+    console.log = function () {
+        for (let i = 0; i < arguments.length; i++) {
+            if (typeof arguments[i] == 'object') {
+                logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(arguments[i], undefined, 2) : arguments[i]) + '<br />';
+            } else {
+                logger.innerHTML += arguments[i] + '<br />';
+            }
+            // logger.innerHTML += arguments[i] + '<br />';
+        }
+        old(...arguments);
+    }
+})();
---
+++ readme.md
@@ -0,0 +1,1 @@
+# JS Homevork library
